c31b0930b2fffc165f7c36b45a3bd15f66910514
Tanvir Rahman <tanvirrahman@pop-os.localdomain>
2020-08-06 14:12:23 +0600

    added

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    "mongourl" : "mongodb://localhost:27017/todoapp"
+    "mongourl" : "mongodb+srv://trahman123456:trahman123456@cluster0.v1x5w.mongodb.net/todo?retryWrites=true&w=majority"
 }
